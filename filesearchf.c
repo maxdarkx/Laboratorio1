3964b4ddb12de828059f049564f97d57056cc630
maxdarkx <max_darkoriginal@hotmail.com>
2018-03-17 20:02:27 -0500

    padding corregido, subprograma para el llenado de la matriz listo. Ahora hay que verificar que este bien

--- filesearchf.c
+++ filesearchf.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-#include <stdint.h>
+
 #include <inttypes.h>
 #include <stdlib.h>
 #define l_int_size 64
 
 void hex2bin(uint64_t *data,int size,int **val); //funcion para convertir datos de hexadecimal a binario
 void datashow(uint64_t *data,int size);  //funcion para mostrar los datos contenidos en un archivo en hexadecimal
-int archivo(char archivo[30],uint64_t *txt, int *size); //funcion para leer un archivo y convertirlo a datos binarios
+uint64_t* archivo(char archivo[30],uint64_t *txt, int *size); //funcion para leer un archivo y convertirlo a datos binarios
 uint64_t doblar(uint64_t d); //gira los datos para que queden en el orden requerido mas adelante
-
+int string(uint64_t *a,int size,int act,uint64_t* b); //funcion que inserta el string a(directo del archivo, con padding) en un arreglo b de 25 posiciones de 64 bits
+void stateArray(uint64_t *s,int size,uint64_t **a);  //funcion para crear un state Array... la famosa Matriz M(w(x+5y)+z)
 
 //programa principal. Se deben crear dos punteros: uno para los datos en 64 bits(data) y otro para los datos binarios (bin)
 //se debe realizar las asignaciones dinamicas desde aca y no en las funciones secundarias.
@@ -17,36 +18,39 @@ int main()
 {
     char arch[]="archivo.txt";
     uint64_t *data;
+    uint64_t *S;
     //int **bin;
     int size=0;
+    int act=0;
 
     //bin=(int **) malloc(sizeof(int *));
-    data=(uint64_t *) malloc(sizeof(uint64_t));
 
-    archivo(arch,data,&size);
-
-
-    if(data!=NULL)
+    
+    if(data==NULL)
     {
-        /*datashow(data,size);
-
-        bin=(int**) malloc(size*sizeof(int *));
-        for(int i=0;i<size;i++)
-        {
-            bin[i]=(int*)malloc(l_int_size*sizeof(int));
-        }
-
-
-        hex2bin(data,size,bin);*/
+        printf("ERROR\n");
     }
     else
     {
-        printf("NULL");
+        printf("OK");
     }
+    S=(uint64_t *)calloc(25,sizeof(uint64_t));
+
+    data=archivo(arch,data,&size);
+    printf("\nVECTOR A:\n");
+    datashow(data,size);
+    act=string(data,size,act,S);
+    if(data!=NULL)
+    {    
+        printf("\nVECTOR A:\n");
+    }
+    
+    printf("\nVECTOR B:\n");
+    datashow(S,25);
 
     printf("\n");
 
-    free(data);
+    //free(data);
     return 0;
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -77,8 +81,9 @@ void datashow(uint64_t *data,int size)  //funcion para mostrar los datos conteni
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-int archivo(char archivo[30],uint64_t *txt, int *size) //funcion para leer un archivo y convertirlo a datos binarios
+uint64_t* archivo(char archivo[30],uint64_t *txt, int *size) //funcion para leer un archivo y convertirlo a datos binarios
 {
     FILE *arch;
 
@@ -87,7 +92,7 @@ int archivo(char archivo[30],uint64_t *txt, int *size) //funcion para leer un ar
     int pbits;            //indica  la cantidad de pedazos de 8 bits que quedan sobrando
     int p64;               //indicador de pedazos de 64 bits
     uint8_t *resto;         //puntero de 8 bits, ideal para recolectar los datos sobrantes de 8 bits
-    
+    int cbits;              //cantidad de bits del string tomado del archivo + 8 bits por el padding
 
     arch=fopen(archivo,"rb");       //se lee el nombre del archivo y si es correcto:
 
@@ -95,64 +100,55 @@ int archivo(char archivo[30],uint64_t *txt, int *size) //funcion para leer un ar
     {
         fseek(arch,0,SEEK_END);     //se lee todo el archivo para verificar la cantidad de datos contenidos
         j=ftell(arch);              //y se guarda en j
-
-        pbits=((j*8)%64)/8;         //se calcula la cantidad de datos huerfanos de 8 bits
-        p64=8*j/64;                 //asi como la cantidad de datos de 64 bits
+        cbits=j+1;
+        pbits=((cbits*8)%64)/8;         //se calcula la cantidad de datos huerfanos de 8 bits
+        p64=8*cbits/64;                 //asi como la cantidad de datos de 64 bits
 
         rewind(arch);               //se devuelve el puntero al inicio del archivo
 
-        txt=(uint64_t *)realloc(txt,(p64+1)*sizeof(uint64_t));      //se asigna un espacio en memoria segun la cantidad de datos que tenga el archivo
-        resto=(uint8_t *) malloc(pbits*sizeof(uint8_t));			//p64+2 posiciones por un posible error de padding
+        txt=(uint64_t *)realloc(txt,(p64)*sizeof(uint64_t));      //se asigna un espacio en memoria segun la cantidad de datos que tenga el archivo
+        resto=(uint8_t *) malloc((pbits+1)*sizeof(uint8_t));			//p64 posiciones por un posible error de padding
 
         fread(txt,sizeof(uint64_t),p64,arch);       //se lee la cantidad de datos que se puedan almacenar en variables de 64 bits
-        fread(resto,sizeof(uint8_t),pbits,arch);    // y de 8 bits huerfanas
+        
+        
 
         if (pbits>0)    //si pbits==0, no hay datos huerfanos. pero si los hay, hay que meterlos a la variable de 64 bits
         {
             p64++;
-        }
-        for(i=0;i<pbits;i++)
-        {
-            txt[p64-1]=(txt[p64-1]|resto[i]);       //se utiliza una or y sucesivas rotaciones de datos hacia la izquierda
-            if(i<pbits-1)                           //para meter los datos huerfanos
+            fread(resto,sizeof(uint8_t),pbits,arch);    // y de 8 bits huerfanas
+            resto[pbits-1]=0xF8;
+            for(i=0;i<pbits;i++)
             {
-                txt[p64-1]=txt[p64-1]<<8;
+                
+                if(i<pbits)                           //para meter los datos huerfanos
+                {
+                    txt[p64-1]=txt[p64-1]<<8;
+                }
+                txt[p64-1]=(txt[p64-1]|resto[i]);       //se utiliza una or y sucesivas rotaciones de datos hacia la izquierda
+            }
+            for(i=0;i<p64-1;i++)            //girar los datos de 64 bits para que aparezcan en el orden correcto
+            {                               //OJO: si el dato es huerfano, ya esta ordenado. por eso solo se ejecuta en p64-1 datos
+                txt[i]=doblar(txt[i]);
             }
         }
-
-        
-        for(i=0;i<p64-1;i++)			//girar los datos de 64 bits para que aparezcan en el orden correcto
-        {								//OJO: si el dato es huerfano, ya esta ordenado. por eso solo se ejecuta en p64-1 datos
-        	txt[i]=doblar(txt[i]);
-        }
-
-
-
-
-
-
-        printf("Datos de 64:%d, Datos sobrantes: %d, Bytes de archivo: %d\t\n",p64-1,pbits,j);
-        datashow(txt,p64);
-
-        
-	   for(i=pbits;i<l_int_size;i++)    //se agregan los datos del padding
+        else
         {
-            txt[p64-1]=(txt[p64-1]|resto[i]);
-
-            if(i<(pbits+8))
-             {
-                txt[p64-1]=txt[p64-1]<<1;
-                if(i>=pbits && i<=pbits+4)
-                {
-                    txt[p64-1]=txt[p64-1]| 1;
-                    printf("datos1: %"PRIx64"\n",txt[p64-1]);
-                }
-                else
-                {
-                    printf("datos0: %"PRIx64"\n",txt[p64-1]);
-                }
-             }
+            resto[0]=0xF8;
+            for(i=0;i<p64;i++)            
+            {                               
+                txt[i]=doblar(txt[i]);
+            }
+            //txt[p64-1]=txt[p64-1]<<8;
+            txt[p64-1]=(txt[p64-1]|resto[0]);
         }
+        
+
+        
+
+
+        printf("Datos de 64:%d, Datos sobrantes: %d, Bytes de archivo: %d\t\n",p64,pbits,cbits);
+        datashow(txt,p64);
 
        *size=p64;
         out=0;
@@ -162,9 +158,9 @@ int archivo(char archivo[30],uint64_t *txt, int *size) //funcion para leer un ar
         //printf("No se encuentra el archivo");
         out=1; //si hay algun error con la lectura del archivo se retorna 1
     }
-    free(resto);
+    //free(resto);
     fclose(arch);
-    return out;
+    return txt;
 
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -244,3 +240,36 @@ uint64_t doblar(uint64_t d) //gira los datos para que queden en el orden requeri
 	return temp3;
 }
 
+
+void stateArray(uint64_t *s,int size,uint64_t **a)  //funcion para crear un state Array... la famosa Matriz M(w(x+5y)+z)
+{
+    int i=0,j=0,k=0;
+
+    for(i=0;i<5;i++)
+    {
+        for(j=0;j<5;j++)
+        {
+            a[j][i]=s[k];
+            k++;
+        }
+    }
+}
+
+int string(uint64_t *a,int size,int act,uint64_t* b) //funcion que inserta el string a(directo del archivo, con padding) en un arreglo b de 25 posiciones de 64 bits
+{
+    int i,j;
+
+    for (i=0;i<25;i++)
+    {
+        if(i<size)
+        {
+            b[i]=a[act+i];
+        }
+        else if(i=24)
+        {
+            b[i]=0x1;
+        }
+        printf("(%d)b: %"PRIx64"\n",act+i,b[i]);
+    }
+    return act+i;
+}
