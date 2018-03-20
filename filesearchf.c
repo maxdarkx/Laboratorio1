e6ecb2108f04a6902071895b29353733032b930c
maxdarkx <max_darkoriginal@hotmail.com>
2018-03-19 21:50:07 -0500

    casi listo... solo faltan detalles menores

--- filesearchf.c
+++ filesearchf.c
@@ -4,21 +4,26 @@
 #include <stdlib.h>
 #define l_int_size 64
 
+
 void hex2bin(uint64_t data,int *val); //funcion para convertir datos de hexadecimal a binario
 void datashow(uint64_t *data,int size);  //funcion para mostrar los datos contenidos en un archivo en hexadecimal
-void stateArrayShow(int ***A); //Muestra el contenido de una matriz tipo State Array
-uint64_t* archivo(char archivo[30], int *size); //funcion para leer un archivo y convertirlo a datos binarios
+void stateArrayShow(uint64_t **A); //Muestra el contenido de una matriz tipo State Array
+uint64_t* archivo(char archivo[30],uint64_t *txt, int size); //funcion para leer un archivo y convertirlo a datos binarios
 uint64_t doblar(uint64_t d); //gira los datos para que queden en el orden requerido mas adelante
 int string(uint64_t *a,int size,int act,uint64_t* b); //funcion que inserta el string a(directo del archivo, con padding) en un arreglo b de 25 posiciones de 64 bits
-void stateArray(uint64_t *s,int ***a);  //funcion para crear un state Array... la famosa Matriz M(w(x+5y)+z)
-void theta(int **C,int **D,int ***S,int ***S1);
+void stateArray(uint64_t *s,uint64_t **a);  //funcion para crear un state Array... la famosa Matriz M(w(x+5y)+z)
+void theta(uint64_t **S,uint64_t **S1);
 int mod(int x,int n);
+int tamano(char archivo[]);
 
-void Theta 	(int ***S,  int ***S1);
-void Rho 	(int ***S1, int ***S2);
-void Pi 	(int ***S2, int ***S3);
-void Chi 	(int ***S3, int ***S4);
-void Iota	(int ***S4, int ***SF, int r);
+
+
+void Theta 	(uint64_t **S,  uint64_t **S1);
+void Rho 	(uint64_t **S1, uint64_t **S2);
+void Pi 	(uint64_t **S2, uint64_t **S3);
+void Chi 	(uint64_t **S3, uint64_t **S4);
+void Iota	(uint64_t **S4, uint64_t **SF, int r);
+void Ronda ();
 
 const uint64_t RC[25]={ 0x0000000000000001,	0x0000000000008082,	0x800000000000808A,	0x8000000080008000,	0x000000000000808B,
 						0x0000000080000001, 0x8000000080008081, 0x8000000000008009, 0x000000000000008A, 0x0000000000000088,
@@ -38,135 +43,56 @@ int main()
     char arch[]="archivo.txt";		//archivo del cual se lee el mensaje
     uint64_t *data;					//puntero que recoge los datos que hay en el mensaje
     uint64_t *St;					//String que recibe los datos del mensaje y lo convierte en cadenas de 1600 bits con padding
-    int j;
+    int i,j;
     //int **bin;
-    int size=0;						//tamano del String
+    int size=0,p64;						//tamano del String
     int act=0;						//posicion en la que se esta trabajando en el String
     
 //variables necesarias para las transformaciones
 
-    int k,r,t;
+    int k,r,t;    
+    uint64_t **S;
+    uint64_t **SF;
     
-    int ***S;
-    int ***S1;
-    int ***S2;
-    int ***S3;
-    int ***S4;
-    int ***S5;
-    int **D;
-    int **C;
-
-    S = (int ***) malloc (5*sizeof(int ***));
-    for (r = 0; r< 5; r++)
-    {
-        S[r] = (int **) malloc(5*sizeof(int*));
-        for (k = 0; k < 5; k++)
-        {
-            S[r][k] = (int*)malloc(64*sizeof(int));
-        }
-    }
-
-    S1 = (int ***) malloc (5*sizeof(int ***));
-    for (r = 0; r< 5; r++)
-    {
-        S1[r] = (int **) malloc(5*sizeof(int*));
-        for (k = 0; k < 5; k++)
-        {
-            S1[r][k] = (int*)malloc(64*sizeof(int));
-        }
-    }
-
-    S2 = (int ***) malloc (5*sizeof(int ***));
-    for (r = 0; r< 5; r++)
-    {
-        S2[r] = (int **) malloc(5*sizeof(int*));
-        for (k = 0; k < 5; k++)
-        {
-            S2[r][k] = (int*)malloc(64*sizeof(int));
-        }
-    }
-
-    S3 = (int ***) malloc (5*sizeof(int ***));
-    for (r = 0; r< 5; r++)
-    {
-        S3[r] = (int **) malloc(5*sizeof(int*));
-        for (k = 0; k < 5; k++)
-        {
-            S3[r][k] = (int*)malloc(64*sizeof(int));
-        }
-    }
-
-    S4 = (int ***) malloc (5*sizeof(int ***));
-    for (r = 0; r< 5; r++)
-    {
-        S4[r] = (int **) malloc(5*sizeof(int*));
-        for (k = 0; k < 5; k++)
-        {
-            S4[r][k] = (int*)malloc(64*sizeof(int));
-        }
-    }
-
-    S5 = (int ***) malloc (5*sizeof(int ***));
-    for (r = 0; r< 5; r++)
-    {
-        S5[r] = (int **) malloc(5*sizeof(int*));
-        for (k = 0; k < 5; k++)
-        {
-            S5[r][k] = (int*)malloc(64*sizeof(int));
-        }
-    }
-
     //bin=(int **) malloc(sizeof(int *));
 
 
     St=(uint64_t *)calloc(25,sizeof(uint64_t));
 
+    S = (uint64_t **) malloc (5*sizeof(uint64_t*));
+    for (r = 0; r< 5; r++)
+    {
+        S[r] = (uint64_t *) malloc (5*sizeof(uint64_t));
+    }
 
-    data=archivo(arch,&size);
+    SF = (uint64_t **) malloc (5*sizeof(uint64_t*));
+    for (r = 0; r< 5; r++)
+    {
+        SF[r] = (uint64_t *) malloc (5*sizeof(uint64_t));
+    }
 
-    //printf("\nVECTOR A:\n");
-    //datashow(data,size);
 
-    act=string(data,size,act,St);
+    size=tamano(arch);
+    p64=8*size/64+1;
+    data=(uint64_t *) calloc(p64,sizeof(uint64_t));
 
+    archivo(arch,data,size);
+    if(p64==0)
+    {
+        p64++;
+    }
+    printf("\nVECTOR A:\n");
+    datashow(data,p64);
+
+    act=string(data,p64,act,St);
+
+    printf("\nString\n");
     datashow(St,25);
 
     printf("\nStateArray\n");
     stateArray(St,S);
-
     stateArrayShow(S);
-
-    printf("\nTheta\n");
-    Theta( S,S1);
-    stateArrayShow(S1);
-
-    Rho(S1,S2);
-    printf("\nSALIDA RHO\n");
-    stateArrayShow(S2);
-
-    Pi(S2,S3);
-    printf("\nSALIDA PI\n");
-    stateArrayShow(S3);
-
-    Chi(S3,S4);
-    printf("\nSALIDA CHI\n");
-    stateArrayShow(S4);
-
-
-    Iota(S4,S5,1);
-
-
-
-
-
-
-
-
-
-
-
-
-
+    Ronda(S,SF);
     //free(data);
     return 0;
 }
@@ -177,7 +103,6 @@ int main()
 void datashow(uint64_t *data,int size)  //funcion para mostrar los datos contenidos en un archivo en hexadecimal
 {
     int i=0;
-
     for(i=0;i<size;i++)
     {
         if(i==0)
@@ -199,8 +124,109 @@ void datashow(uint64_t *data,int size)  //funcion para mostrar los datos conteni
 
 
 
+void Ronda(uint64_t **S, uint64_t **SF)
+{
+    uint64_t **S1;
+    uint64_t **S2;
+    uint64_t **S3;
+    uint64_t **S4;
+    uint64_t **S5;
+    int r=0,j=0,i=0;
+
+    S1 = (uint64_t **) malloc (5*sizeof(uint64_t*));
+    for (r = 0; r< 5; r++)
+    {
+        S1[r] = (uint64_t *) malloc(5*sizeof(uint64_t));
+    }
+
+    S2 = (uint64_t **) malloc (5*sizeof(uint64_t*));
+    for (r = 0; r< 5; r++)
+    {
+        S2[r] = (uint64_t *) malloc(5*sizeof(uint64_t));
+    }
+
+    S3 = (uint64_t **) malloc (5*sizeof(uint64_t*));
+    for (r = 0; r< 5; r++)
+    {
+        S3[r] = (uint64_t *) malloc(5*sizeof(uint64_t));
+    }
+
+    S4 = (uint64_t **) malloc (5*sizeof(uint64_t*));
+    for (r = 0; r< 5; r++)
+    {
+        S4[r] = (uint64_t *) malloc(5*sizeof(uint64_t));
+    }
+
+    S5 = (uint64_t **) malloc (5*sizeof(uint64_t*));
+    for (r = 0; r< 5; r++)
+    {
+        S5[r] = (uint64_t *) malloc(5*sizeof(uint64_t));
+    }
+
+    for(r=0;r<24;r++)
+    {
+        if(r==0)
+        {
+            for(i=0;i<5;i++)
+            {
+                for(j=0;j<5;j++)
+                {
+                    S5[i][j]=S[i][j];
+                }
+            }
+        }
+
+        Theta( S5,S1);
+        Rho(S1,S2);
+        Pi(S2,S3);
+        Chi(S3,S4);
+        Iota(S4,S5,r);
+
+        if(r==0 || r==23 )
+        {
+            printf("Ronda %d",r);            
+            printf("\nSALIDA THETA\n");
+            stateArrayShow(S1);
+
+            printf("\nSALIDA RHO\n");
+            stateArrayShow(S2);
+    
+            printf("\nSALIDA PI\n");
+            stateArrayShow(S3);
+
+            printf("\nSALIDA CHI\n");
+            stateArrayShow(S4);
+
+            printf("\nSALIDA IOTA\n");
+            stateArrayShow(S5);
+        }
+
+    }
+}
+
+
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-uint64_t* archivo(char archivo[30], int *size) //funcion para leer un archivo y convertirlo a datos binarios
+
+int tamano(char archivo[])
+{
+    int j;
+    FILE *arch;
+    arch=fopen(archivo,"rb");       //se lee el nombre del archivo y si es correcto:    
+    
+    fseek(arch,0,SEEK_END);     //se lee todo el archivo para verificar la cantidad de datos contenidos
+    j=ftell(arch);              //y se guarda en j
+    
+    fclose(arch);
+    return j;
+}
+
+
+
+
+
+
+uint64_t* archivo(char archivo[30],uint64_t *txt, int size) //funcion para leer un archivo y convertirlo a datos binarios
 {
     FILE *arch;
 
@@ -210,65 +236,65 @@ uint64_t* archivo(char archivo[30], int *size) //funcion para leer un archivo y 
     int p64;               //indicador de pedazos de 64 bits
     uint8_t *resto;         //puntero de 8 bits, ideal para recolectar los datos sobrantes de 8 bits
     int cbits;              //cantidad de bits del string tomado del archivo + 8 bits por el padding
-    uint64_t* txt;			//puntero que recoge los datos del archivo para ser leidos de a 64 bits por vez
+    //uint64_t* txt;			//puntero que recoge los datos del archivo para ser leidos de a 64 bits por vez
 
     arch=fopen(archivo,"rb");       //se lee el nombre del archivo y si es correcto:
 
     if(arch!=NULL)
     {
-        fseek(arch,0,SEEK_END);     //se lee todo el archivo para verificar la cantidad de datos contenidos
-        j=ftell(arch);              //y se guarda en j
-        cbits=j+1;
-        pbits=((cbits*8)%64)/8;         //se calcula la cantidad de datos huerfanos de 8 bits
-        p64=8*cbits/64;                 //asi como la cantidad de datos de 64 bits
+        //cbits=j+1;
+        pbits=((size*8)%64)/8;         //se calcula la cantidad de datos huerfanos de 8 bits
+        p64=8*size/64;                 //asi como la cantidad de datos de 64 bits
 
-        rewind(arch);               //se devuelve el puntero al inicio del archivo
-
-        txt=(uint64_t *)calloc(p64,sizeof(uint64_t));      //se asigna un espacio en memoria segun la cantidad de datos que tenga el archivo
         resto=(uint8_t *) malloc((pbits+1)*sizeof(uint8_t));			//p64 posiciones por un posible error de padding
 
         fread(txt,sizeof(uint64_t),p64,arch);       //se lee la cantidad de datos que se puedan almacenar en variables de 64 bits
 
+        if(p64==0)
+        {
+            pbits++;
+        }
 
 
         if (pbits>0)    //si pbits==0, no hay datos huerfanos. pero si los hay, hay que meterlos a la variable de 64 bits
         {
             p64++;
+            
             fread(resto,sizeof(uint8_t),pbits,arch);    // y de 8 bits huerfanas
-            resto[pbits-1]=0xF8;
-            for(i=0;i<pbits;i++)
+            resto[pbits]=0x1F;
+            
+
+            for(i=0;i<=pbits;i++)
             {
 
+                
+                txt[p64-1]=(txt[p64-1]|((uint64_t)resto[pbits-i]));       //se utiliza una or y sucesivas rotaciones de datos hacia la izquierda
                 if(i<pbits)                           //para meter los datos huerfanos
                 {
                     txt[p64-1]=txt[p64-1]<<8;
                 }
-                txt[p64-1]=(txt[p64-1]|resto[i]);       //se utiliza una or y sucesivas rotaciones de datos hacia la izquierda
+                printf("\nAqui\n");
+                printf("%"PRIx64"\n",(uint64_t)resto[pbits-i]);
+                printf("%"PRIx64"\n",txt[p64-1]);
+            
             }
-            for(i=0;i<p64-1;i++)            //girar los datos de 64 bits para que aparezcan en el orden correcto
-            {                               //OJO: si el dato es huerfano, ya esta ordenado. por eso solo se ejecuta en p64-1 datos
-                txt[i]=doblar(txt[i]);
-            }
+            //txt[p64-1]=doblar(txt[p64-1]);
+            
         }
         else
         {
-            resto[0]=0xF8;
-            for(i=0;i<p64;i++)
+            resto[0]=0x1F;
+            
+            /*for(i=0;i<p64;i++)
             {
                 txt[i]=doblar(txt[i]);
-            }
+            }*/
             //txt[p64-1]=txt[p64-1]<<8;
             txt[p64-1]=(txt[p64-1]|resto[0]);
         }
 
-
-
-
-
-        printf("Datos de 64:%d, Datos sobrantes: %d, Bytes de archivo: %d\t\n",p64,pbits,cbits);
-        //datashow(txt,p64);
-
-       *size=p64;
+        printf("Datos de 64:%d, Datos sobrantes: %d, Bytes de archivo: %d\t\n",p64,pbits,size);
+        datashow(txt,p64);
         out=0;
     }
     else
@@ -332,24 +358,11 @@ uint64_t doblar(uint64_t d) //gira los datos para que queden en el orden requeri
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void stateArray(uint64_t *s,int ***A)  //funcion para crear un state Array... la famosa Matriz M(w(x+5y)+z)
+void stateArray(uint64_t *s,uint64_t **a)  //funcion para crear un state Array... la famosa Matriz M(w(x+5y)+z)
 {
 
-    int i=0,j=0,k=0,l=0;
-    uint64_t **a;
-    int *bin;
-
-
-    a = (uint64_t **) malloc(5*sizeof(uint64_t *));
-    for (i = 0; i < 5; i++)
-    {
-        a[i] = (uint64_t *)malloc(64*sizeof(uint64_t));
-    }
-
-    bin = (int *) malloc(64*sizeof(int));
-
-
-
+    int i=0,j=0,k=0;
+    
     for(i=0;i<5;i++)
     {
         for(j=0;j<5;j++)
@@ -358,18 +371,6 @@ void stateArray(uint64_t *s,int ***A)  //funcion para crear un state Array... la
             k++;
         }
     }
-
-
-    for(i=0;i<5;i++)
-    {
-        for(j=0;j<5;j++)
-        {
-            hex2bin(a[i][j],bin);
-            for(l=0;l<64;l++)
-            A[i][j][l]=bin[l];
-        }
-    }
-
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -381,107 +382,80 @@ int string(uint64_t *a,int size,int act,uint64_t* b) //funcion que inserta el st
 
     for (i=0;i<25;i++)
     {
+
         if(i<size)
         {
-            b[i]=a[act+i];
+            if (i<17)
+            {
+                printf("\nsize=%d,%"PRIx64,size,a[act+i]);
+                b[i]=a[act+i];
+            }
         }
-        else if(i=24)
+        
+        if(i==(16))
         {
-            b[i]=0x1;
+            b[i]=b[i]|0x8000000000000000;
         }
         //printf("(%d)b: %"PRIx64"\n",act+i,b[i]);
     }
     return act+i;
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-void stateArrayShow(int ***A) //Muestra el contenido de una matriz tipo State Array
+void stateArrayShow(uint64_t **A) //Muestra el contenido de una matriz tipo State Array
 {
 
-    int i,j,k,l;
+    int i,j;
     uint64_t z=0;
 
     printf("{\n");
-    l=0;
     for(i=0;i<5;i++)
     {
         printf("[\n ");
         for(j=0;j<5;j++)
         {
-            z=0;
-            for(k=0;k<64;k++)
-            {
-                z=A[i][j][k] | z;
-                if(k<63)
-                {
-                	z=z<<1;
-                }
-                //printf("%d:(%d,%"PRIx64")\n",k,A[i][j][k],z);
-            }
-            printf("%"PRIx64" ",z);
+            printf("%"PRIx64" ",A[i][j]);
         }
         printf("];\n");
     }
     printf("{\n\n\n");
 }
 
-
-void Theta(int ***S,int ***S1)
+void Theta(uint64_t **S,uint64_t **S1)
 {
-	int **C;
-	int **D;
+	uint64_t *C;
+	uint64_t *D;
 	int k1;
     int x,y,z;
     int n,m,o;
 
 
-    C = (int **) malloc(5*sizeof(int *));
-    for (k1 = 0; k1 < 5; k1++)
-    {
-        C[k1] = (int *)malloc(64*sizeof(int));
-    }
-
-    D = (int **) malloc(5*sizeof(int *));
-    for (k1 = 0; k1 < 5; k1++)
-    {
-        D[k1] = (int *)malloc(64*sizeof(int));
-    }
+    C = (uint64_t *) malloc(5*sizeof(uint64_t ));
+    
+    D = (uint64_t *) malloc(5*sizeof(uint64_t ));
+    
 
 
 
     for(x = 0;x<5;x++)
-    {
-        for (z = 0; z < 64; z++)
-        {
-            C[x][z] = S[x][0][z]^S[x][1][z]^S[x][2][z]^S[x][3][z]^S[x][4][z];
-        }
+    { 
+       C[x] = S[x][0]^S[x][1]^S[x][2]^S[x][3]^S[x][4];
     }
-
+    
     for(x = 0;x<5;x++)
     {
-        for (z = 0; z < 64; z++)
-        {
-            //m=mod(x-1,5);
-            //n=mod(x+1,5);
-            //o=mod(z-1,64);
-
-            D[x][z] = C[mod(x-1,5)][z]^C[mod(x+1,5)][mod(z-1,64)];
-
-        }
+        D[x] = C[mod(x-1,5)]^(C[mod(x+1,5)]>>63|C[mod(x+1,5)]<<1);
     }
     for(x = 0;x<5;x++)
     {
         for (y = 0; y < 5; y++)
         {
-            for (z = 0; z < 64; z++)
-            {
-                S1 [x][y][z] = S[x][y][z] ^ D[x][z];
-            }
+            S1 [x][y] = S[x][y] ^ D[x];       
         }
     }
 }
 
 
-void Rho(int ***S1, int ***S2)
+void Rho(uint64_t **S1, uint64_t **S2)
 {
 
 	int x=1;
@@ -489,12 +463,10 @@ void Rho(int ***S1, int ***S2)
 	int z=0;
 	int t=0;
 	int x1,y1;
+    int mov=0;
 
-
-    for (z = 0; z < 64; z++) 
-    {
-    	S2 [0][0][z] = S1[0][0][z];
-    }
+    S2 [0][0] = S1[0][0];
+    
 
     for (t = 0; t <24; t++) 
     {
@@ -503,17 +475,17 @@ void Rho(int ***S1, int ***S2)
     		x=x1;
     		y=y1;
     	}
-	    for (z = 0; z < 64; z++) 
-	    {
-        	S2 [x][y][z] = S1[x][y][mod(z-(t+1)*(t+2)/2,64)];
-       	}
-       	y1 = mod((2*x+3*y),5);
+        mov=(t+1)*(t+2)/2;
+
+	    S2 [x][y] = S1[x][y]<<mov | S1[x][y]>>64-mov;
+       	
+
+        y1 = mod((2*x+3*y),5);
         x1 = y;
         
-    }
-    
+    }  
 }
-void Pi(int ***S2,int ***S3)
+void Pi(uint64_t **S2,uint64_t **S3)
 {
     int x;
 	int y;
@@ -523,41 +495,45 @@ void Pi(int ***S2,int ***S3)
 	{
        for (y = 0; y < 5; y++) 
        {
-           	for (z = 0; z < 64; z++) 
-           	{
-           		S3[x][y][z] = S2[mod(x+3*y,5)][x][z] ;
-
-           	}
+           	
+           	S3[x][y] = S2[mod(x+3*y,5)][x] ;
        }
    }
+   
 }
 
 
-void Chi(int ***S3,int ***S4)
+void Chi(uint64_t **S3,uint64_t **S4)
 {
-	int x,y,z;
+	int x,y;
 
 	for(x = 0;x<5;x++)
 	{
         for (y = 0; y < 5; y++) 
         {
-           for (z = 0; z < 64; z++) 
-           {
-	           S4 [x][y][z] = S3 [x][y][z] ^( S3 [mod(x+1,5)][y][z] & S3 [mod(x+2,5)][y][z] ) ;
-           }
+	        S4 [x][y] = S3 [x][y] ^ (( S3 [mod(x+1,5)][y] ^ 0XFFFFFFFFFFFFFFFF) & (S3[mod(x+2,5)][y]));
         }
     }
 }
 
-void Iota(int ***S4,int ***SF, int r)
+void Iota(uint64_t **S4,uint64_t **SF, int r)
 {
-	int i,temp;
-	for(i=0;i<64;i++)
-	{
-		temp=(RC[r]>>i)&1;
-		//printf("%dTEMP=%d,%"PRIx64"\n",i,temp,RC[r]);
-		SF[0][0][63-i]=S4[0][0][63-i]^temp; //temp me trae los bits en el orden inverso a como se encuentran en el vector
-	}										//toca usar polvos magicos para hacer la operacion bien.
+	int i,j;
+    uint64_t temp;
+
+    for (i=0;i<5;i++)
+    {
+        for (j=0;j<5;j++)
+        {
+            SF[i][j]=S4[i][j];
+        }
+    }
+
+
+
+	temp=RC[r];
+	
+	SF[0][0]=S4[0][0]^temp; 
 }
 
 int mod(int x,int n)
